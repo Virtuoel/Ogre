9bceba3905c6bb7981d176ccfebeb1d6b8b4b814
Virtuoel <Virtuoel@users.noreply.github.com>
2019-11-21 02:26:43 +0300

    Fix vertical movement when colliding horizontally while in fluid

---
+++ src/main/java/virtuoel/towelette/mixin/ViewableWorldMixin.java
@@ -0,0 +1,42 @@
+package virtuoel.towelette.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.ViewableWorld;
+
+@Mixin(ViewableWorld.class)
+public interface ViewableWorldMixin extends BlockView
+{
+	default boolean intersectsFluid(Box box)
+	{
+		final int minX = MathHelper.floor(box.minX);
+		final int maxX = MathHelper.ceil(box.maxX);
+		final int minY = MathHelper.floor(box.minY);
+		final int maxY = MathHelper.ceil(box.maxY);
+		final int minZ = MathHelper.floor(box.minZ);
+		final int maxZ = MathHelper.ceil(box.maxZ);
+		
+		try(BlockPos.PooledMutable pos = BlockPos.PooledMutable.get())
+		{
+			for (int x = minX; x < maxX; ++x)
+			{
+				for (int y = minY; y < maxY; ++y)
+				{
+					for (int z = minZ; z < maxZ; ++z)
+					{
+						if (!this.getFluidState(pos.method_10113(x, y, z)).isEmpty())
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+		
+		return false;
+	}
+}

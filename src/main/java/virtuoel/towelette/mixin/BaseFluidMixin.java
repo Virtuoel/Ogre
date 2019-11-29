d1a2a36a3182fde8b369280d865c719d7bcee2eb
Virtuoel <Virtuoel@users.noreply.github.com>
2019-11-29 19:48:31 +0300

    Import cleanup

--- src/main/java/virtuoel/towelette/mixin/BaseFluidMixin.java
+++ src/main/java/virtuoel/towelette/mixin/BaseFluidMixin.java
@@ -1,7 +1,5 @@
 package virtuoel.towelette.mixin;
 
-import java.util.Iterator;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -33,8 +31,8 @@ import net.minecraft.world.IWorld;
 import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
 import virtuoel.towelette.Towelette;
-import virtuoel.towelette.api.ModifiableWorldStateLayer;
 import virtuoel.towelette.api.LayerRegistrar;
+import virtuoel.towelette.api.ModifiableWorldStateLayer;
 import virtuoel.towelette.util.FluidUtils;
 import virtuoel.towelette.util.StateNeighborGroup;
 

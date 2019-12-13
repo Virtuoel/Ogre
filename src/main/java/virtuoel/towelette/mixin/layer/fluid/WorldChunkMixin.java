9f243af8244e03ecb160085013a1481a6a50c7a9
Virtuoel <Virtuoel@users.noreply.github.com>
2019-12-14 02:01:01 +0300

    Clean up imports

--- src/main/java/virtuoel/towelette/mixin/layer/fluid/WorldChunkMixin.java
+++ src/main/java/virtuoel/towelette/mixin/layer/fluid/WorldChunkMixin.java
@@ -1,7 +1,5 @@
 package virtuoel.towelette.mixin.layer.fluid;
 
-import javax.annotation.Nullable;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,17 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FluidBlock;
-import net.minecraft.state.State;
-import net.minecraft.util.crash.CrashException;
-import net.minecraft.util.crash.CrashReport;
-import net.minecraft.util.crash.CrashReportSection;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.WorldChunk;
-import virtuoel.towelette.api.ChunkSectionStateLayer;
 import virtuoel.towelette.api.ChunkStateLayer;
-import virtuoel.towelette.api.LayerData;
 import virtuoel.towelette.api.LayerRegistrar;
 
 @Mixin(WorldChunk.class)
